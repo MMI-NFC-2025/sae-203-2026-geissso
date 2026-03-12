70933aef2985a28f4f2f159175bf101874b78658
Geiss Solène <solene.geiss@edu.univ-fcomte.fr>
2026-03-12 11:45:37 +0100

    mise en ligne du pocketbase

--- backend/backend.mjs
+++ backend/backend.mjs
@@ -1,6 +1,6 @@
 import PocketBase from 'pocketbase';
 
-const db = new PocketBase("http://127.0.0.1:8090");
+const db = new PocketBase("https://pbflytapfest.geiss-solene.fr");
 
 export async function getScene() {
   const scenes = await db.collection('Scene').getFullList({
